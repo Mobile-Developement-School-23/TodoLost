3b8994430c0451ca821c3930c6754bd49cca0c17
Dmitriy Danilin <danilindn@icloud.com>
2023-06-10 16:45:30 +0300

    Реализовать структуру TodoItem

--- TodoLost/Services/Network/TodoItem.swift
+++ TodoLost/Services/Network/TodoItem.swift
@@ -1,12 +1,35 @@
-//
-//  TodoItem.swift
-//  TodoLost
-//
-//  Created by Дмитрий Данилин on 10.06.2023.
-//
-
 import Foundation
 
+enum Importance {
+    case low
+    case normal
+    case important
+}
+
 struct TodoItem {
+    let id: String
+    let text: String
+    let importance: Importance
+    let deadline: Date?
+    let isDone: Bool
+    let dateCreated: Date
+    let dateEdited: Date?
     
+    init(
+        id: String = UUID().uuidString,
+        text: String,
+        importance: Importance,
+        deadline: Date?,
+        isDone: Bool,
+        dateCreated: Date = Date(),
+        dateEdited: Date?
+    ) {
+        self.id = id
+        self.text = text
+        self.importance = importance
+        self.deadline = deadline
+        self.isDone = isDone
+        self.dateCreated = dateCreated
+        self.dateEdited = dateEdited
+    }
 }
