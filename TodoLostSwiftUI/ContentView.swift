d7b38e9bc8a60daa2116ca6a2973309af34977eb
Dmitriy Danilin <danilindn@icloud.com>
2023-07-15 00:53:07 +0300

    Проба создания списка

--- TodoLostSwiftUI/ContentView.swift
+++ TodoLostSwiftUI/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+            .navigationTitle("Мои дела")
         }
-        .padding()
     }
 }
 
